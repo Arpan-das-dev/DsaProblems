2318275550662af0981a051b128e7b5a8eadf36e
Arpan-das-dev <dasarpan915@gmail.com>
2025-12-31 17:56:48 +0530

    add instance of the classes required for testing with values and tests all three questions solved today

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
+import Hashing.ContainsDuplicate;
+import Stack.ValidParenthesis;
+import TwoPointers.ValidPalindrome;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ValidPalindrome palindrome = new ValidPalindrome();
+        ContainsDuplicate duplicate = new ContainsDuplicate();
+        System.out.println(palindrome.palindrome("race a car"));
+        System.out.println("31/12/2025 next contains duplicate -->");
+        int [] duplicateArray = {1,2,3,4};
+        System.out.println("Ans -> "+duplicate.containsDuplicate(duplicateArray));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        ValidParenthesis parenthesis = new ValidParenthesis();
+        System.out.println("31/12/2025 check if a string contains valid parenthesis");
+        System.out.println(parenthesis.isValid("([)]"));
     }
 }
