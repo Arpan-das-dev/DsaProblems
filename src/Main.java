8fd19c3e4080d4e26de8091ac6a40f10faf02d4a
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-04 12:05:55 +0530

    create the object for class @LongestConsecutiveSequence.java and test the method with dummy result

--- src/Main.java
+++ src/Main.java
@@ -45,6 +45,10 @@ public class Main {
         System.out.println();
 
         LongestConsecutiveSequence consecutiveSequence = new LongestConsecutiveSequence();
+        System.out.println("04/01/2026 solved to find longest consecutive sequence inside an array");
+        System.out.println(consecutiveSequence.longestConsecutive(new int[] {100,4,200,1,3,2}));
+        System.out.println();
+
         DecodeWays ways = new DecodeWays();
     }
 }
