baaa81bafaccb68dd0e41a8f0d2ce80039b8d4e8
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-09 23:28:02 +0530

    Add modified binary search for rotated sorted array (SearchInRotatedSortedArray class)

--- src/BinarySearch/SearchInRotatedSortedArray.java
+++ src/BinarySearch/SearchInRotatedSortedArray.java
@@ -1,40 +1,70 @@
 package BinarySearch;
 
+/**
+ * Utility class for searching a target value in a rotated sorted array
+ * using a modified binary search.
+ *
+ * <p>The input array is originally sorted in ascending order, but then rotated
+ * at an unknown pivot. The goal is to find the index of the target in O(log n)
+ * time, or return -1 if it does not exist.</p>
+ *
+ * <p><b>Approach:</b></p>
+ * <ul>
+ *   <li>Use binary search with two pointers: left and right.</li>
+ *   <li>At each step, determine which half (left or right) is sorted.</li>
+ *   <li>If the target lies within the sorted half, discard the other half;
+ *       otherwise search in the unsorted half.</li>
+ * </ul>
+ *
+ * <p>This leverages the fact that in a rotated sorted array, at least one side
+ * of the midpoint is always sorted, allowing logarithmic search time.[web:36][web:39]</p>
+ *
+ * <p><b>Time Complexity:</b> O(log n), where n is the number of elements in the array.<br>
+ * <b>Space Complexity:</b> O(1), since only a few pointers and variables are used.[web:36]</p>
+ *
+ * @author Arpan Das
+ * @since 09/01/2026
+ */
 public class SearchInRotatedSortedArray {
 
-    // [4,5,6,7,0,1,2]
-    public int search(int[] num , int target) {
-      int n = num.length-1;
-      int left = 0; int right = n;
-      while (left<=right) {
-          int mid = left + (right - left)/2;
-          if(num[mid] == target) return mid;
+    /**
+     * Searches for a target value in a rotated sorted array.
+     *
+     * <p>If the target exists, its index is returned; otherwise, -1 is returned.
+     * The search is performed using a modified binary search that accounts for
+     * the rotation.[web:36][web:39]</p>
+     *
+     * @param num    a rotated sorted array of integers
+     * @param target the value to search for
+     * @return the index of the target if found; otherwise -1
+     */
+    public int search(int[] num, int target) {
+        int left = 0;
+        int right = num.length - 1;
 
-          if(num[left] <= num[mid]) // discard the left part because it's sorted
-          {
-              /*
-              lets assume mid = 3, target is 5
-              num[left] = 4 < target && 5 < num[mid] = 7
-               */
-              if(num[left] <= target && target < num[mid]) {
-                  right = mid-1; // right = mid-1 = 2[index] = 6
-              } else {
-                  left = mid + 1; // left = mid + 1 = 4 [index] = 0 which is invalid here
-              }
-          }
-          else {
-              /*
-              assume mid = 5; target = 0;
-              num[mid] = 1 < target && 1 <= num [right] 2
-               */
-              if(num[mid] < target && target <= num[right]) {
-                  left = mid + 1;
-              } else {
-                  right = mid -1;
-              }
-          }
-      }
-      return -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (num[mid] == target) return mid;
+
+            // Left half is sorted
+            if (num[left] <= num[mid]) {
+                // Target lies within the sorted left half
+                if (num[left] <= target && target < num[mid]) {
+                    right = mid - 1;
+                } else { // Search in the right half
+                    left = mid + 1;
+                }
+            } else { // Right half is sorted
+                // Target lies within the sorted right half
+                if (num[mid] < target && target <= num[right]) {
+                    left = mid + 1;
+                } else { // Search in the left half
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
     }
 
 }
