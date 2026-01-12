baa58ba40d53bebdc9e6aa0e4a4e46a5920a8ac2
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-12 14:03:57 +0530

    add solution to find max value we can rob where house are in circular phase using house robber1 logic

--- src/DynamicProgramming/HouseRobber2.java
+++ src/DynamicProgramming/HouseRobber2.java
@@ -1,4 +1,29 @@
 package DynamicProgramming;
 
 public class HouseRobber2 {
+    public  int rob(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+        
+        int pickFirst = robLinearHouses(nums,0,nums.length-1);
+        int pickLast = robLinearHouses(nums,1,nums.length);
+
+        return Math.max(pickFirst,pickLast);
+    }
+
+    private int robLinearHouses(int[] nums, int start, int limit) {
+        int lastDay = 0;
+        int twoDay = 0;
+
+        for (int i = start; i < limit; i++) {
+            int pick = twoDay + nums[i];
+            int skip = lastDay;
+
+            int current = Math.max(pick,skip);
+
+            twoDay = lastDay;
+            lastDay = current;
+        }
+        return lastDay;
+    }
 }
