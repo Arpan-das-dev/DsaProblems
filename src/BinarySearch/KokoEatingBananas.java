cd6a51eee27d283377267607d05e090c28eea341
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-12 14:03:10 +0530

    add solution to find the min time koko needs to eat bananas

--- src/BinarySearch/KokoEatingBananas.java
+++ src/BinarySearch/KokoEatingBananas.java
@@ -1,4 +1,25 @@
 package BinarySearch;
 
 public class KokoEatingBananas {
+    public int minEatingSpeed(int[] piles, int h) {
+        int minSpeed = 1; // default speed can eat one per hour
+        int maxSpeed = 0; // max speed == max item of the array;
+        for (int banana : piles){
+            maxSpeed = Math.max(banana,maxSpeed); // assign the max item of the array;
+        }
+        while (minSpeed < maxSpeed) { // running the binary search
+            int mid = minSpeed + (maxSpeed-minSpeed)/2; // calculating mid position
+            if(canEat(mid,piles,h)) maxSpeed = mid; // he can eat let's try with smaller.
+            else minSpeed = mid + 1; // too slow update the pointer
+        }
+        return minSpeed; // as per question requested to get min speed
+    }
+
+    private boolean canEat(int speed, int[] piles, int time) {
+        int timeTaken = 0;
+        for (int banana : piles) {
+            timeTaken += (int) Math.ceil((double) banana /speed); // updating the time values with ceil values
+        }
+        return timeTaken<=time; // if > return false if <=  returns true;
+    }
 }
