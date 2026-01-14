33fa8cc7ade764eea7131ebf4e50138235106759
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-14 19:58:12 +0530

    add solution to find diameter of a binary tree with helper DFS

--- src/Trees/DiameterOfATree.java
+++ src/Trees/DiameterOfATree.java
@@ -1,4 +1,20 @@
 package Trees;
 
 public class DiameterOfATree {
+
+    private int diameter;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null) return 0;
+        maxDepth(root);
+        return diameter;
+    }
+
+    private int maxDepth(TreeNode node){
+        if(node == null) return 0;
+        int leftDepth = maxDepth(node.left);
+        int rightDepth = maxDepth(node.right);
+
+        diameter = Math.max(diameter,rightDepth+leftDepth);
+        return Math.max(leftDepth,rightDepth)+1;
+    }
 }
