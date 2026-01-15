644d4eeb6bc52e26a2a22afcd7c3d341240437e0
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-15 20:44:36 +0530

    add solution to reverse a linked list

--- src/LinkedLists/ReverseLinkedList.java
+++ src/LinkedLists/ReverseLinkedList.java
@@ -1,4 +1,17 @@
 package LinkedLists;
 
 public class ReverseLinkedList {
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
+
+        ListNode prev = null;
+        ListNode current = head;
+        while(current!= null) {
+           ListNode temp = current.next; // get the next value
+           current.next = prev; // set the next value as previous
+           prev = current; // reverse the current
+           current = temp; // assign the value as next node
+        }
+        return prev;
+    }
 }
