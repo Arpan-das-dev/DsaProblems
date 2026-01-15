8cb76acaf694867a5cb1b92a302426c6a2d6439d
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-15 20:43:36 +0530

    add method to solve if a binary tree is balanced or not

--- src/Trees/BalancedBinaryTree.java
+++ src/Trees/BalancedBinaryTree.java
@@ -1,4 +1,23 @@
 package Trees;
 
 public class BalancedBinaryTree {
+
+    public boolean isBalanced(TreeNode root) {
+        return DFS(root) != -1;
+
+    }
+    private int DFS (TreeNode root) {
+        if(root == null) return 0;
+
+        int left = DFS(root.left);
+        if(left == -1) return -1;
+
+        int right = DFS(root.right);
+        if(right == -1) return -1;
+
+       if(Math.abs(left - right) > 1) {
+           return -1;
+       }
+        return Math.max(left,right)+1;
+    }
 }
