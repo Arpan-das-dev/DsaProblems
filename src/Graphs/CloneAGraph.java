a8f9e2dcf4a05f7a5ffb3c8fecd94e895c7d1b2b
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-16 19:24:09 +0530

    add solution to clone a graph

--- src/Graphs/CloneAGraph.java
+++ src/Graphs/CloneAGraph.java
@@ -1,4 +1,23 @@
 package Graphs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CloneAGraph {
+    private final Map<Node,Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if(node == null) return null;
+        if(visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        Node clone = new Node(node.val);
+        visited.put(node,clone);
+
+        for (Node neighbor : node.neighbors) {
+            clone.neighbors.add(cloneGraph(neighbor));
+        }
+        return clone;
+    }
 }
