0f2147538c3637346177c0bdfdd0eecff64d5fe4
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-16 19:26:21 +0530

    add solution to solve a reverse polish notation equation

--- src/Stack/ReversePolishNotation.java
+++ src/Stack/ReversePolishNotation.java
@@ -1,4 +1,25 @@
 package Stack;
 
+import java.util.*;
+
 public class ReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        Deque<Integer> numeric = new ArrayDeque<>();
+        
+        //["10","6","9","3","+","-11","*","/","*","17","+","5","+"]
+        for (String item : tokens) {
+            if (item.equals("+") || item.equals("-") || item.equals("*") || item.equals("/")) {
+                int lastPrevious = numeric.pop();
+                int secondPrevious = numeric.pop();
+                switch (item) {
+                    case "-" -> numeric.push(secondPrevious - lastPrevious);
+                    case "*" -> numeric.push(secondPrevious * lastPrevious);
+                    case "/" -> numeric.push(secondPrevious / lastPrevious);
+                    default -> numeric.push(secondPrevious + lastPrevious);
+                }
+            }
+            else numeric.push(Integer.parseInt(item));
+        }
+        return numeric.pop();
+    }
 }
