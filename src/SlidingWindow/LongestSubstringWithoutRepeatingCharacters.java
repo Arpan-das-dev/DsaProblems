4ddba884ead0bcfe13958f96e0e93c966511e039
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-17 22:27:31 +0530

    add solution with sliding window pattern to find the longest substring with non repeating chars

--- src/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
+++ src/SlidingWindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -1,4 +1,23 @@
 package SlidingWindow;
 
-public class LongestSubstringWithoutRepeatatingCharacters {
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> characterSet = new HashSet<>();
+        int length = 0;
+        int left = 0;
+
+        for(int right = 0; right < s.length(); right ++) {
+            Character ch = s.charAt(right);
+            while (characterSet.contains(ch)) {
+                characterSet.remove(s.charAt(left));
+                left++;
+            }
+            characterSet.add(ch);
+            length = Math.max(length,(right - left + 1));
+        }
+        return length;
+    }
 }
