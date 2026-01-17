3c817be00798bad2b487849a2e7f5eb687c9c1c0
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-17 22:29:55 +0530

    add reccursive solution to find if one tree is a subtree of another

--- src/Trees/SubTree.java
+++ src/Trees/SubTree.java
@@ -1,4 +1,18 @@
 package Trees;
 
 public class SubTree {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if(root == null) return false;
+        if(isSameTree(root,subRoot)) return true;
+
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+    }
+    private boolean isSameTree(TreeNode treeNode, TreeNode sub) {
+        if(treeNode == null && sub == null) return  true;
+        if(treeNode == null || sub == null) return false;
+
+        if (treeNode.val != sub.val) return false;
+
+        return isSameTree(treeNode.left,sub.left) && isSameTree(treeNode.right,sub.right);
+    }
 }
