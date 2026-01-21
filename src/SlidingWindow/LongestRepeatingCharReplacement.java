87d8ecd0c89e6a6e64a2cf20be65f369921ed795
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-21 13:54:13 +0530

    add sliding solution to find longest repeating chars replacement

--- src/SlidingWindow/LongestRepeatingCharReplacement.java
+++ src/SlidingWindow/LongestRepeatingCharReplacement.java
@@ -1,4 +1,26 @@
 package SlidingWindow;
 
 public class LongestRepeatingCharReplacement {
+    public int characterReplacement(String s, int k) {
+       int left = 0;
+       int maxFreq = 0;
+       int maxLength = 0;
+       int [] charCount = new int[26];
+
+       for (int right = 0; right<s.length(); right++) {
+           char currentChar = s.charAt(right);
+
+           charCount[currentChar - 'A']++;
+           maxFreq = Math.max(maxFreq,charCount[currentChar-'A']);
+
+           int window = right - left +1;
+
+           if(window-maxFreq > k) {
+               charCount[s.charAt(left)-'A'] --;
+               left++;
+           }
+           maxLength = Math.max(maxFreq,right-left+1);
+       }
+       return maxLength;
+    }
 }
