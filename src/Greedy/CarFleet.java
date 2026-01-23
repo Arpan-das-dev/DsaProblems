c19983fa1edbe32a5cbc07550fb09cd5e18a4968
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-23 16:36:13 +0530

    add sloution to count car fleets by greedy algo

--- src/Greedy/CarFleet.java
+++ src/Greedy/CarFleet.java
@@ -1,20 +1,30 @@
-package Stack;
+package Greedy;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Stack;
 
 public class CarFleet {
-    // target = 12
-    //position = [10, 8, 0, 5, 3]
-    //            2, 4, 12, 7,9
-    //speed   = [2, 4, 1, 1, 3]
-    // 1,1,7,3,12  1,1,3,7,12
+
     public int carFleet(int target, int[] position, int[] speed) {
-        double[] times = new double[position.length];
-        for (int i = 0; i < position.length; i++) {
-            times[i] = ((double) (target - position[i]) /speed[i]);
+        int n = position.length;
+        int [][] cars = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            cars[i][0]= position[i];
+            cars[i][1]= speed[i];
         }
-        Stack<Double> fleets = new Stack<>();
+
+        Arrays.sort(cars,(a,b)-> b[0]-a[0]);
+
+        int fleets = 0;
+        double lastTime = 0.0;
+
+        for (int i = 0; i < n; i++) {
+            double time = (double) (target - cars[i][0]) / cars[i][1];
+            if (time > lastTime) {
+                fleets++;
+                lastTime = time;
+            }
+        }
+        return fleets;
     }
 }
