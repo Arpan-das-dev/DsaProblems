5e33571751bc03211fd4a5be628e1c97a3653c16
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-26 17:07:38 +0530

    add partial optimal and fully optimal solution to find missing number from an array

--- src/Mathematical/MissingNumber.java
+++ src/Mathematical/MissingNumber.java
@@ -1,4 +1,14 @@
 package Mathematical;
 
 public class MissingNumber {
+    public int missingNumber (int [] nums) {
+        // simple mathematical equation for sum of n numbers of natural numbers;
+        // sum = (n*(n+1))/2;
+        int n = nums.length;
+        int sum = (n * (n+1)) /2;
+        for(int value : nums) {
+            sum -= value;
+        }
+        return sum;
+    }
 }
