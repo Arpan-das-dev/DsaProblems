5efb91300376d4090e2055b2f2667e2200d4587f
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-29 18:27:35 +0530

    add recursive dfs solution to find max no of island present

--- src/Graphs/NumberOfIsland.java
+++ src/Graphs/NumberOfIsland.java
@@ -1,4 +1,32 @@
 package Graphs;
 
 public class NumberOfIsland {
+
+    public int numIslands(char[][] grid) {
+        int island = 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if(grid[r][c] == '1') {
+                    island++;
+                    dfs(r,c,rows,cols,grid);
+                }
+            }
+        }
+        return island;
+    }
+
+    private void dfs(int r, int c, int rows, int cols, char[][] grid) {
+        if (r < 0 || r >= rows || c < 0 || c >= cols) return;
+        if (grid[r][c] == '0') return;
+
+        grid[r][c] = '0';
+
+        dfs(r, c + 1, rows, cols, grid);
+        dfs(r, c - 1, rows, cols, grid);
+        dfs(r + 1, c, rows, cols, grid);
+        dfs(r - 1, c, rows, cols, grid);
+    }
 }
