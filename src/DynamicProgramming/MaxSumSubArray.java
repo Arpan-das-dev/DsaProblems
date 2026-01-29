ff70a363468f876d37409e6462aa4141674b58e6
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-29 18:19:47 +0530

    add DP solution to find max product of subArray

--- src/DynamicProgramming/MaxSumSubArray.java
+++ src/DynamicProgramming/MaxSumSubArray.java
@@ -1,4 +1,19 @@
 package DynamicProgramming;
 
 public class MaxSumSubArray {
+    public int maxSubArray(int[] nums) {
+        if(nums == null || nums.length == 0) return 0;
+        if(nums.length == 1) return nums[0];
+
+        // at current index max sum of the subArray
+        int subSumEnds = nums[0];
+        // max subArray seen so far
+        int maxSumSeen = nums[0];
+
+        for (int index = 1; index < nums.length; index++) {
+            subSumEnds = Math.max(nums[index],subSumEnds+nums[index]);
+            maxSumSeen = Math.max(subSumEnds,maxSumSeen);
+        }
+        return maxSumSeen;
+    }
 }
