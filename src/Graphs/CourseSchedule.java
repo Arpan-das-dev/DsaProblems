f8e2ea938fc6674f5075191ca2cf74c2a5e85e57
Arpan-das-dev <dasarpan915@gmail.com>
2026-01-31 19:00:14 +0530

    add tropological sort technique to find if the course is complitable or not

--- src/Graphs/CourseSchedule.java
+++ src/Graphs/CourseSchedule.java
@@ -1,4 +1,36 @@
 package Graphs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class CourseSchedule {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            graph.add(new ArrayList<>());
+        }
+        int [] inDegree = new int[numCourses];
+        for (int [] pre : prerequisites) {
+            graph.get(pre[1]).add(pre[0]);
+            inDegree[pre[0]]++; // to complete 2nd first complete 1;
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++) {
+            if(inDegree[i] == 0) queue.offer(i);
+        }
+
+        int count = 0;
+        while (!queue.isEmpty()){
+            int curr = queue.poll();
+            count++;
+            for (int next : graph.get(curr)){
+                inDegree[next]--;
+                if(inDegree[next] == 0) queue.offer(next);
+            }
+        }
+        return count == numCourses;
+    }
 }
