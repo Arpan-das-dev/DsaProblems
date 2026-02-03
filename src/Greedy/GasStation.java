c437b816f7f0dc66d4e18f04f3e02a0b42b8ebb1
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-03 19:49:38 +0530

    add greedy solution to find the starting point of gas station

--- src/Greedy/GasStation.java
+++ src/Greedy/GasStation.java
@@ -1,4 +1,23 @@
 package Greedy;
 
 public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int totalSpent = 0;
+        int totalFuelHas = 0;
+        int startPoint = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            totalSpent += gas[i] - cost[i];
+            totalFuelHas += gas[i] - cost[i];
+
+            // if the tank has negative fuel it can't start from here
+            if(totalFuelHas < 0) {
+                // reset the start point and amount of fuel it has
+                totalFuelHas = 0;
+                startPoint = i+1;
+            }
+        }
+        return totalSpent >= 0 ? startPoint : -1;
+    }
+
 }
