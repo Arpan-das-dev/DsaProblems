2037b75d6134e6dfa9a9d5b79354ab1587906eb0
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-16 19:59:05 +0530

    add solution for reverse bit by bitmanupulation

--- src/BitManipulation/ReverseBits.java
+++ src/BitManipulation/ReverseBits.java
@@ -1,4 +1,24 @@
 package BitManipulation;
 
 public class ReverseBits {
+    public int reverseBits(int n) {
+        int result = 0;
+        for (int shift = 31; shift >= 0; shift--) {
+            // shifting left from current position
+            result += (n & 1) << shift;
+            // shifting n's bits by one (on right)
+            n = n >> 1;
+        }
+        return result;
+    }
+
+    public int reverseBitsBitSafeApproach(int n) {
+        int result = 0;
+        for (int shift = 31; shift >= 0; shift--) {
+            result |= (n & 1) << shift;
+            n = n >>> 1;
+        }
+        return result;
+    }
+
 }
