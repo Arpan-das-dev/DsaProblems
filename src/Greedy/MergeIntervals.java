6cf7ac071c7eb3d3327cfb9064a17a0a089715dc
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-17 21:12:04 +0530

    add greedy solution for merge intervals

--- src/Greedy/MergeIntervals.java
+++ src/Greedy/MergeIntervals.java
@@ -1,4 +1,28 @@
 package Greedy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 public class MergeIntervals {
+    public int[][] merge(int[][] intervals){
+        if(intervals == null || intervals.length <= 1) return intervals;
+        Arrays.sort(intervals, Comparator.comparingInt(a -> a[0]));
+
+        List<int[]> merge = new ArrayList<>();
+        int [] previousEnd = intervals[0];
+
+        for (int index = 1; index < intervals.length; index++) {
+            int[] curr = intervals[index];
+            if(previousEnd[1] >= curr[0]) {
+                previousEnd[1] = Math.max(previousEnd[1], curr[1]);
+            } else {
+                merge.add(previousEnd);
+                previousEnd = curr;
+            }
+        }
+        merge.add(previousEnd);
+        return merge.toArray(new int[merge.size()][]);
+    }
 }
