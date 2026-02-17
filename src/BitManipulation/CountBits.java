fa10a165fe762d2c75e5d8826b9f856370c2d527
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-17 21:17:06 +0530

    add bit manupulation to count bits

--- src/BitManipulation/CountBits.java
+++ src/BitManipulation/CountBits.java
@@ -1,4 +1,12 @@
 package BitManipulation;
 
 public class CountBits {
+    public int[] countBits(int n) {
+        int [] result = new int[n+1];
+
+        for (int i = 1; i <= n; i++) {
+            result[i] = result[i & (i-1)] +1;
+        }
+        return result;
+    }
 }
