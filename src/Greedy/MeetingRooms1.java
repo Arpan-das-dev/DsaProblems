2f32d7cb557062242ec3644f19edce8dcf3f7f14
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-18 19:08:41 +0530

    add greedy solution to find if one can join all meetings or not

--- src/Greedy/MeetingRooms1.java
+++ src/Greedy/MeetingRooms1.java
@@ -1,4 +1,19 @@
 package Greedy;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class MeetingRooms1 {
+    public boolean canAttend(int[][] arr) {
+        if(arr == null ||arr.length == 1) return true;
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+
+        int prevEnd = arr[0][1];
+        for (int i = 1; i < arr.length; i++) {
+            int [] curr = arr[i];
+            if(prevEnd > curr[0]) return false;
+            else prevEnd = curr[1];
+        }
+        return true;
+    }
 }
