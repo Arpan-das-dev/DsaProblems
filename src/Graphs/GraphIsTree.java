bf20f26f385a7429aea79116552a0b82868961df
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-20 18:50:52 +0530

    add solution to check if a graph is a tree or not using DFS

--- src/Graphs/GraphIsTree.java
+++ src/Graphs/GraphIsTree.java
@@ -1,4 +1,39 @@
 package Graphs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraphIsTree {
+    public boolean isTree(int n, int m, ArrayList<ArrayList<Integer>> edges) {
+        if(m != n-1) return false;
+
+        ArrayList<List<Integer>> adjcent = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            adjcent.add(new ArrayList<>());
+        }
+
+         edges.forEach(item-> {
+             int u = item.get(0);
+             int v = item.get(1);
+             adjcent.get(u).add(v);
+             adjcent.get(v).add(u);
+         });
+
+        boolean [] visited = new boolean[n];
+        dfs(adjcent,visited,0);
+
+        for (boolean visit : visited){
+            if(!visit) return false;
+        }
+        return true;
+    }
+
+    private void dfs(ArrayList<List<Integer>> adjcent, boolean[] visited, int index) {
+        visited[index] = true;
+
+        for(Integer neighbour : adjcent.get(index)){
+            if(!visited[neighbour]) dfs(adjcent,visited,neighbour);
+        }
+    }
 }
