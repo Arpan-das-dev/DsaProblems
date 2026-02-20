7618244efa25c0d26b78b9c282193af69ff7eaf3
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-20 18:55:30 +0530

    add greedy solution with two pointer to find min rooms required to conduct all meetings

--- src/Greedy/MeetingRooms2.java
+++ src/Greedy/MeetingRooms2.java
@@ -1,4 +1,26 @@
 package Greedy;
 
+import java.util.Arrays;
+
 public class MeetingRooms2 {
+    public int minMeetingRooms(int[] start, int[] end) {
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        int st = 0, en = 0;
+        int rooms = 0, maxRooms = 0;
+
+        while (st<start.length) {
+            // if start time is less than ending then new room required
+            if(start[st] < end[en]) {
+                rooms++;
+                st++;
+            }else {
+                rooms --;
+                en++;
+            }
+            maxRooms = Math.max(maxRooms,rooms);
+        }
+        return maxRooms;
+    }
 }
