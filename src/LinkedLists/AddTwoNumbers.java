5a59731729b29f27b85ef756cf596980e5b20cee
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-22 19:35:35 +0530

    add solution sum -> carry method to add values of two integers

--- src/LinkedLists/AddTwoNumbers.java
+++ src/LinkedLists/AddTwoNumbers.java
@@ -1,4 +1,27 @@
 package LinkedLists;
 
 public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode result = new ListNode(-1);
+        ListNode dummy = result;
+
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0){
+            int sum = carry;
+
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            if(l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum/10;
+            dummy.next = new ListNode(sum % 10);
+            dummy = dummy.next;
+        }
+        return result.next;
+    }
 }
