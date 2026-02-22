612cfea857380bb44c9fdf059322ffad31c07413
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-22 19:39:59 +0530

    add bit manipulation (right shift) with removing trailing 0s to find max distance between two 1s

--- src/BitManipulation/BinaryGap.java
+++ src/BitManipulation/BinaryGap.java
@@ -1,4 +1,22 @@
 package BitManipulation;
 
+/**
+ * this is not from {@code neetcode 150} sheet
+ */
 public class BinaryGap {
+    public int binaryGap(int n) {
+       n >>= Integer.numberOfTrailingZeros(n);
+        if(n == 1) return 0;
+
+       int gap = 0, max = 0;
+       while (n > 0){
+           if((n & 1) == 1) {
+               max = Math.max(gap,max);
+               gap = 0;
+           }
+           else gap++;
+           n = n>>1;
+       }
+       return max+1;
+    }
 }
