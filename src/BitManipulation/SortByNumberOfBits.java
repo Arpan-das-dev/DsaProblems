afd1d601ad94ce4c29887baa58a38ad7ab06ecf5
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-26 22:08:40 +0530

    add solution to sort numbers based on their bits count if tie then actual value

--- src/BitManipulation/SortByNumberOfBits.java
+++ src/BitManipulation/SortByNumberOfBits.java
@@ -1,4 +1,22 @@
 package BitManipulation;
 
+import java.util.*;
+
 public class SortByNumberOfBits {
+    public int[] sortByBits(int[] arr) {
+        if(arr == null || arr.length == 0) return new int[] {};
+        if(arr.length == 1) return arr;
+        Integer[] nums = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            nums[i] = arr[i];
+        }
+        Arrays.sort(nums,(a,b)-> {
+            int c = Integer.bitCount(a) - Integer.bitCount(b);
+            return c != 0 ? c : a-b;
+        });
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i];
+        }
+        return arr;
+    }
 }
