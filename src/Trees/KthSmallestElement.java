ee49ce0f2dbfb1baedd6993b58846277e92b5885
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-26 22:25:35 +0530

    add dfs left -> root -> right to find kth smallest element

--- src/Trees/KthSmallestElement.java
+++ src/Trees/KthSmallestElement.java
@@ -1,4 +1,23 @@
 package Trees;
 
 public class KthSmallestElement {
+    public int kthSmallest(TreeNode root, int k) {
+        int [] count = new int[1];
+        int [] result = new int[1];
+        inorder(root,k,count,result);
+        return result[0];
+    }
+
+    private void inorder(TreeNode root, int k, int[] count, int[] result) {
+        if(root == null) return;
+
+        inorder(root.left,k,count,result);
+        count[0] ++;
+        if(count[0] == k){
+            result[0] = root.val;
+            return;
+        }
+
+        inorder(root.right,k,count,result);
+    }
 }
