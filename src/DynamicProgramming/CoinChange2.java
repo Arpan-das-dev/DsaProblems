4990e35719e7f2a64b2f67260b1bd130edd68862
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-26 22:34:34 +0530

    add dp solution to find all possible way to reach given amount

--- src/DynamicProgramming/CoinChange2.java
+++ src/DynamicProgramming/CoinChange2.java
@@ -1,4 +1,17 @@
 package DynamicProgramming;
 
 public class CoinChange2 {
+    public int change(int amount, int[] coins) {
+        if(coins == null || coins.length == 0) return 0;
+
+        int[] dp = new int[amount+1];
+        dp[0] = 1;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = dp[i] + dp[i-coin];
+            }
+        }
+        return dp[amount];
+    }
 }
