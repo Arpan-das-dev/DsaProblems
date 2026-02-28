0c5627b8072b155d81604af0ca21db0e4601b925
Arpan-das-dev <dasarpan915@gmail.com>
2026-02-28 21:19:51 +0530

    add 2dp solution to find if the expression matches with the string or not

--- src/BitManipulation/ConcatenationOfConsecutiveBinaryNumbers.java
+++ src/BitManipulation/ConcatenationOfConsecutiveBinaryNumbers.java
@@ -1,4 +1,16 @@
 package BitManipulation;
 
-public class ConcatinationOfConsecutiveBinaryNumbers {
+public class ConcatenationOfConsecutiveBinaryNumbers {
+    public int concatenatedBinary(int n) {
+        if(n <=1) return n;
+
+        final int MOD = 1_000_000_007;
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int shift = 32 -Integer.numberOfLeadingZeros(i);
+            result = ((result << shift) | i) % MOD;
+        }
+        return result;
+    }
 }
