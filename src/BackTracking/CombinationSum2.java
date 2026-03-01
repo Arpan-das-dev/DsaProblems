c8c4149a246d742ba3c8100b5e3353822ffa2570
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-01 19:53:16 +0530

    add backtracking solution to solve combination sum2 without duplicates

--- src/BackTracking/CombinationSum2.java
+++ src/BackTracking/CombinationSum2.java
@@ -1,4 +1,33 @@
 package BackTracking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class CombinationSum2 {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(candidates == null || candidates.length == 0) return result;
+        Arrays.sort(candidates);
+        backTracking(candidates,target,0,result,new ArrayList<Integer>());
+        return result;
+    }
+
+    private void backTracking(int[] candidates, int target,
+                              int start, List<List<Integer>> result, ArrayList<Integer> combination)
+    {
+        if(target == 0) {
+            result.add(new ArrayList<>(combination));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if(i > start && candidates[i] == candidates[i-1]) continue;
+            int curr = candidates[i];
+            if(curr > target) break;
+            combination.add(curr);
+            backTracking(candidates,target-curr,i+1,result,combination);
+            combination.removeLast();
+        }
+    }
 }
