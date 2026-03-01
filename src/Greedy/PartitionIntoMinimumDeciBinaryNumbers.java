7cf57a4429d86d56fbee704ead4bee28c4d1dbdb
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-01 20:04:23 +0530

    add greedy based approach solution to find max possible paritions

--- src/Greedy/PartitionIntoMinimumDeciBinaryNumbers.java
+++ src/Greedy/PartitionIntoMinimumDeciBinaryNumbers.java
@@ -1,4 +1,11 @@
 package Greedy;
 
 public class PartitionIntoMinimumDeciBinaryNumbers {
+    public int minPartitions(String n) {
+        int max = 0;
+        for (char ch : n.toCharArray()) {
+            max = Math.max(max,ch - '0');
+        }
+        return max;
+    }
 }
