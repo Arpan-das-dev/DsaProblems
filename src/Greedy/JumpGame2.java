d70b4d58a57dbe84de90579317511206b6caba78
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-04 18:59:50 +0530

    add greddy solution to find min steps required to reach the last index of the array

--- src/Greedy/JumpGame2.java
+++ src/Greedy/JumpGame2.java
@@ -1,4 +1,21 @@
 package Greedy;
 
 public class JumpGame2 {
+    public int jump(int[] nums) {
+        if(nums == null || nums.length <= 1) return 0;
+
+        int maxReach = 0;
+        int steps = 0;
+        int lastJump = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            int curr = nums[i];
+            maxReach = Math.max(maxReach,i+curr);
+            if(i == lastJump){
+                steps++;
+                lastJump = maxReach;
+            } if(lastJump >= nums.length-1) return steps;
+        }
+        return steps;
+    }
 }
