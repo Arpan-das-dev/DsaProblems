59d9d3fe5ba4aaae481fd00ec9bd0c2292194512
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-04 19:04:43 +0530

    add simulation with carry propagation to solve multiply string

--- src/Simulation/MultiplyStrings.java
+++ src/Simulation/MultiplyStrings.java
@@ -1,4 +1,37 @@
 package Simulation;
 
 public class MultiplyStrings {
+    public String multiply(String num1, String num2) {
+        if(num1.equals("0") || num2.equals("0")) return "0";
+
+        int l1 = num1.length();
+        int l2 = num2.length();
+
+        int[] products = new int[l1+l2];
+
+        for (int i = l1-1; i >= 0 ; i--) {
+            int a = num1.charAt(i)-'0';
+            for (int j = l2-1; j >= 0 ; j--) {
+                int b = num2.charAt(j) - '0';
+
+                int product = a * b;
+
+                int high = i+j;
+                int low = i+j+1;
+
+                int sum = products[low]+product;
+
+                products[low] = sum % 10;
+                products[high] += sum/10;
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int num : products) {
+            if (!(sb.isEmpty() && num == 0)) {
+                sb.append(num);
+            }
+        }
+        return sb.toString();
+    }
 }
