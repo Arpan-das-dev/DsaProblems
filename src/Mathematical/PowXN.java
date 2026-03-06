5c95234bfa4c3f282a05a346615391ad78a99c0a
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-06 19:33:32 +0530

    add solution with math logic with bit manipulation to find n exp value of x

--- src/Mathematical/PowXN.java
+++ src/Mathematical/PowXN.java
@@ -1,4 +1,22 @@
 package Mathematical;
 
 public class PowXN {
+    public double myPow(double x, int n) {
+        long pow = n;
+        double result = 1.0;
+
+        if(n < 0) {
+            x = 1/x;
+            pow = -pow;
+        }
+
+        while (pow > 0) {
+            if((pow&1)==1) {
+                result *= x;
+            }
+                x *= x;
+                pow >>=1;
+        }
+        return result;
+    }
 }
