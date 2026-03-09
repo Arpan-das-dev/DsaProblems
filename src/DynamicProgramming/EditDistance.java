bbb02ed5f26b91bf6be285540378b1a40ccab805
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-09 13:44:59 +0530

    add 2D dp solution to find min steps required to edit string 1 to string 2

--- src/DynamicProgramming/EditDistance.java
+++ src/DynamicProgramming/EditDistance.java
@@ -1,4 +1,39 @@
 package DynamicProgramming;
 
+import java.util.Arrays;
+
 public class EditDistance {
+    public int minDistance(String word1, String word2) {
+        int row = word2.length();
+        int col = word1.length();
+
+        int[][] dp = new int[row+1][col+1];
+
+        for (int i = 1; i <= col ; i++) {
+            dp[0][i] = dp[0][i-1]+1;
+        }
+
+        for (int i = 1; i <= row ; i++) {
+            dp[i][0] = dp[i-1][0]+1;
+        }
+        System.out.println(Arrays.deepToString(dp));
+
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+//                System.out.println("word1 -> "+word1.charAt(j-1)+" for j = "+j);
+//                System.out.println("word2 -> "+word2.charAt(i-1));
+                if(word1.charAt(j-1) == word2.charAt(i-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                } else {
+
+                    int top = dp[i-1][j];
+                    int diagonal = dp[i-1][j-1];
+                    int prev = dp[i][j-1];
+                    dp[i][j] = Math.min(prev,Math.min(top,diagonal))+1;
+                }
+            }
+        }
+//        System.out.println(Arrays.deepToString(dp));
+        return dp[row][col];
+    }
 }
