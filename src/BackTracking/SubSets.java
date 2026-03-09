a330f47241f5b0d8181f5af8820f06ddbea17be5
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-09 13:46:42 +0530

    add backtracking solution to genrate all distinct subsets of an array

--- src/BackTracking/SubSets.java
+++ src/BackTracking/SubSets.java
@@ -1,4 +1,26 @@
 package BackTracking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubSets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) return result;
+        backTrack(nums,result,new ArrayList<Integer>(),0);
+        return result;
+    }
+
+    private void backTrack(int[] nums, List<List<Integer>> result, ArrayList<Integer> subset, int start) {
+        if(start == nums.length) {
+            result.add(new ArrayList<>(subset));
+            return;
+        }
+
+        subset.add(nums[start]);
+        backTrack(nums,result,subset,start+1);
+        subset.removeLast();
+        backTrack(nums,result,subset,start+1);
+    }
 }
+// 1,2,3 [],[1],[1,2],[1,2,3],[2
