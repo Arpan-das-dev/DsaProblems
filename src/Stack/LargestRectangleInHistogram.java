e82b3e29aaffd60ce6d6165e6f4915ee32c41bdf
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-10 19:23:42 +0530

    add stack based solution to find largest histogram

--- src/Stack/LargestRectangleInHistogram.java
+++ src/Stack/LargestRectangleInHistogram.java
@@ -1,4 +1,32 @@
 package Stack;
 
+import java.util.Stack;
+
 public class LargestRectangleInHistogram {
+    public int maxArea(int [] heights ) {
+        if(heights == null || heights.length == 0) return 0;
+
+        Stack<Integer> boundaries = new Stack<>();
+        boundaries.push(-1);
+
+        int maxArea = 0;
+
+        for (int i = 0; i < heights.length; i++) {
+            while (boundaries.peek()!=-1 && heights[i] <= heights[boundaries.peek()]) {
+                int height = heights[boundaries.pop()];
+                int left = heights[boundaries.peek()];
+                int width = i-left-1;
+                maxArea = Math.max(maxArea,height*width);
+            }
+            boundaries.push(i);
+        }
+
+        while (boundaries.peek()!= -1) {
+            int height = boundaries.pop();
+            int left = boundaries.peek();
+            int width = heights.length - left - 1;
+            maxArea = Math.max(maxArea,height*width);
+        }
+        return maxArea;
+    }
 }
