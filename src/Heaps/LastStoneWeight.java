5b6f563e61d63df004a3ecd1b308671e9ec3253d
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-11 22:29:41 +0530

    add both(collection version and normal) version to solve last stone weight problem with queues

--- src/Heaps/LastStoneWeight.java
+++ src/Heaps/LastStoneWeight.java
@@ -1,4 +1,39 @@
 package Heaps;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
 public class LastStoneWeight {
+    public int lastStoneWeight(int[] stones) {
+        if(stones == null || stones.length == 0) return 0;
+        if(stones.length == 1) return stones[0];
+
+        PriorityQueue<Integer> stoneQueue = new PriorityQueue<>((a, b)-> b-a);
+        for (int weights : stones) stoneQueue.offer(weights);
+
+        while (stoneQueue.size() > 1){
+            int a = stoneQueue.poll();
+            int b = stoneQueue.poll();
+
+            if(a != b) stoneQueue.offer(a-b);
+        }
+        return stoneQueue.isEmpty() ? 0 : stoneQueue.peek();
+    }
+
+    public int lastStoneWeightCollectionVersion(int[] stones) {
+        if(stones == null || stones.length == 0) return 0;
+        if(stones.length == 1) return stones[0];
+
+        PriorityQueue<Integer> stoneQueue = new PriorityQueue<>(Collections.reverseOrder());
+        stoneQueue.addAll(Arrays.stream(stones).boxed().toList());
+
+        while (stoneQueue.size() > 1){
+            int a = stoneQueue.poll();
+            int b = stoneQueue.poll();
+
+            if(a != b) stoneQueue.offer(a-b);
+        }
+        return stoneQueue.isEmpty() ? 0 : stoneQueue.peek();
+    }
 }
