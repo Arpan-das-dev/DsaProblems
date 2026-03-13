3497edb24d1c9a16ddf36fc075f0f73b89cef589
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-13 19:17:00 +0530

    add hashmap/bit manipulation technique to identify single occurrence of an element

--- src/BitManipulation/SingleNumber.java
+++ src/BitManipulation/SingleNumber.java
@@ -1,4 +1,24 @@
 package BitManipulation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SingleNumber {
+    public int singleNumberMapVersion(int[] nums) {
+        Map<Integer,Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num,freq.getOrDefault(num,0)+1);
+        }
+
+        for(Map.Entry<Integer,Integer> item : freq.entrySet()) {
+            if(item.getValue() == 1) return item.getKey();
+        }
+        return -1;
+    }
+
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) single^=num;
+        return single;
+    }
 }
