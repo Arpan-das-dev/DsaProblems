3ad28565620d65c271c8a444a787f385ae312d32
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-13 19:08:53 +0530

    add backtacking solution to generate all distinct valid palidroming substring

--- src/BackTracking/PalindromePartitioning.java
+++ src/BackTracking/PalindromePartitioning.java
@@ -1,4 +1,47 @@
 package BackTracking;
 
-public class PalindromePartioning {
+import java.util.ArrayList;
+import java.util.List;
+
+public class PalindromePartitioning {
+    public List<List<String>> partition(String s) {
+        List<List<String>> result = new ArrayList<>();
+        if(s == null || s.isEmpty()) return result;
+
+        backTrack(s,result,0,new ArrayList<>());
+        return result;
+    }
+
+    private void backTrack(String s, List<List<String>> result, int i, ArrayList<String> objects) {
+        if(i >= s.length()){
+            result.add(new ArrayList<>(objects));
+            return;
+        }
+
+        for (int len = 1; len+i <= s.length(); len++) {
+            String substring = s.substring(i, i + len);
+
+            if(isValidPalindrome(substring)){
+                objects.add(substring);
+                backTrack(s,result,i+len,objects);
+                objects.removeLast();
+            }
+        }
+    }
+
+
+    private boolean isValidPalindrome(String string) {
+        if(string.length() == 1) return true;
+
+        int left = 0;
+        int right = string.length()-1;
+
+        while (left < right){
+            if(string.charAt(left) != string.charAt(right)) return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
 }
