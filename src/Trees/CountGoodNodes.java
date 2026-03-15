7ca4b6c2f62bb5843e5f47e5aa3c6d94a17a322e
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-15 20:10:32 +0530

    add dfs based solution to count good nodes

--- src/Trees/CountGoodNodes.java
+++ src/Trees/CountGoodNodes.java
@@ -1,4 +1,22 @@
 package Trees;
 
 public class CountGoodNodes {
+
+    public int goodNodes(TreeNode root) {
+        return dfs(root,Integer.MIN_VALUE);
+    }
+
+    private int dfs(TreeNode root, int maxSeenSoFar) {
+        if(root == null) return 0;
+
+        int count = 0;
+        if(root.val >= maxSeenSoFar) {
+            count = 1;
+            maxSeenSoFar = root.val;
+        }
+        count += dfs(root.left,maxSeenSoFar);
+        count += dfs(root.right,maxSeenSoFar);
+
+        return count;
+    }
 }
