35848b44649cd8fa4eca84e0f276bed610e85171
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-17 17:08:18 +0530

    solve subset2 problem with backtracking approach with skipping duplicates by sorting them

--- src/BackTracking/SubSets2.java
+++ src/BackTracking/SubSets2.java
@@ -1,4 +1,28 @@
 package BackTracking;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class SubSets2 {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if(nums == null || nums.length == 0) return result;
+
+        Arrays.sort(nums);
+        backTrack(nums,result,0,new ArrayList<Integer>());
+        return result;
+    }
+
+    private void backTrack(int[] nums, List<List<Integer>> result, int i, ArrayList<Integer> subset) {
+        result.add(new ArrayList<>(subset));
+
+        for (int j = i; j < nums.length; j++) {
+            if (j > i && nums[j] == nums[j - 1]) continue;
+
+            subset.add(nums[j]);
+            backTrack(nums, result, j + 1, subset);
+            subset.removeLast();
+        }
+    }
 }
