08cd0e57018f9971227b9dddc8ea17b04c09de70
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-17 17:20:20 +0530

    implemted DFS method with depth matching with list size to add right viewed nodes in the result list

--- src/Trees/RightSideView.java
+++ src/Trees/RightSideView.java
@@ -1,4 +1,23 @@
 package Trees;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class RightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        dfs(root,result,0);
+        return result;
+    }
+
+    private void dfs(TreeNode root, List<Integer> result,int depth) {
+        if(root == null) return;
+        // it ensures at same level only right node is taken and
+        // in the next step same recursive call with increased depth
+        if(depth == result.size()) result.add(root.val);
+
+        // right dfs first because view is from right side
+        dfs(root.right,result,depth+1);
+        dfs(root.left,result,depth+1);
+    }
 }
