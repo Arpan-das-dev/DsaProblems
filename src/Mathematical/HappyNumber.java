60537e21b35ac17cf269df909893a11d97d09a1f
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-18 20:27:42 +0530

    add mathematical solution to found if number is happy or not

--- src/Mathematical/HappyNumber.java
+++ src/Mathematical/HappyNumber.java
@@ -1,4 +1,29 @@
 package Mathematical;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HappyNumber {
+    public boolean isHappy(int n) {
+        if(n == 1) return true;
+        Set<Integer> integers = new HashSet<>();
+
+        while (n != 1){
+            if(integers.contains(n)) return false;
+
+            integers.add(n);
+            n = getNextInt(n);
+        }
+        return true;
+    }
+
+    private int getNextInt(int n) {
+        int sum = 0;
+        while (n > 0){
+            int digit = n%10;
+            sum += digit * digit;
+            n /= 10;
+        }
+        return sum;
+    }
 }
