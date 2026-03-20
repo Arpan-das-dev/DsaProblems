b833214651172cac69ce6dcc6d8a715cae9f72a9
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-20 22:03:03 +0530

    add solution to find concatination of an array

--- src/Simulation/ConcatenationOfArray.java
+++ src/Simulation/ConcatenationOfArray.java
@@ -1,4 +1,18 @@
 package Simulation;
 
-public class ConcationationOfArray {
+public class ConcatenationOfArray {
+    public int[] getConcatenation(int[] nums) {
+        if(nums == null || nums.length == 0) return new int [] {};
+
+        int length = nums.length;
+        int[] result = new int[length *2];
+
+        for(int i = 0; i<result.length; i++){
+            if(i>=length){
+                result[i] = nums[i-length];
+            }
+            else result[i] = nums[i];
+        }
+        return result;
+    }
 }
