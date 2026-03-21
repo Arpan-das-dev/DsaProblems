4fdc1503fc00faf205924830c39bea15552b9bc9
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-21 19:24:12 +0530

    add prefix sum method to solve the problem

--- src/Hashing/SubArraySumK.java
+++ src/Hashing/SubArraySumK.java
@@ -1,4 +1,28 @@
 package Hashing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SubArraySumK {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int prefix = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            // increase prefix value
+            prefix += num;
+            // now check if the remaining complement part is present or not
+            int key = prefix - k;
+            if (map.containsKey(key)) {
+                // if for such complement we found value then add it
+                count += map.get(key);
+            }
+            // now current prefix will be the next key because it's maybe the complement of another sub array
+            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
+        }
+        return count;
+    }
 }
