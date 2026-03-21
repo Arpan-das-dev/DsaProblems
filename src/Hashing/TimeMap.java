335bbf2fac92c796396f7befd1bf7671f5413a1f
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-21 19:26:59 +0530

    designed a time based key value store

--- src/Hashing/TimeMap.java
+++ src/Hashing/TimeMap.java
@@ -1,4 +1,50 @@
 package Hashing;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class TimeMap {
+    private final Map<String, List<Pair>> pairMap;
+
+    public TimeMap() {
+        this.pairMap = new HashMap<>();
+    }
+
+    public void set(String key, String value, int timestamp) {
+        pairMap
+                .computeIfAbsent(key, k -> new ArrayList<>())
+                .add(new Pair(timestamp, value));
+    }
+
+    public String get(String key, int timestamp) {
+        if (!pairMap.containsKey(key)) return "";
+
+        List<Pair> pairs = pairMap.get(key);
+        String result = "";
+        int left = 0, right = pairs.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (pairs.get(mid).timeStamp <= timestamp) {
+                result = pairs.get(mid).value;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+
+    static class Pair {
+        int timeStamp;
+        String value;
+
+        public Pair(int timeStamp, String value) {
+            this.timeStamp = timeStamp;
+            this.value = value;
+        }
+    }
 }
