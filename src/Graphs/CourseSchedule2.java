0bf9a41cb0abf54258c27f203c8fc97035aff6d9
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-23 22:29:41 +0530

    used topological sort with indegree to return probable schedule

--- src/Graphs/CourseSchedule2.java
+++ src/Graphs/CourseSchedule2.java
@@ -1,4 +1,48 @@
 package Graphs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 public class CourseSchedule2 {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) {
+            adj.add( new ArrayList<>());
+        }
+        int[] dependency = new int[numCourses];
+
+        for (int [] course: prerequisites){
+            int toComplete = course[0];
+            int first = course[1];
+
+            dependency[toComplete]++;
+            adj.get(first).add(toComplete);
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; i++) {
+            if(dependency[i]== 0) queue.offer(i);
+        }
+
+        int count = 0;
+        int [] result = new int[numCourses];
+        int index = 0;
+
+        while (!queue.isEmpty()){
+            int curr = queue.poll();
+            // course completed
+            count ++;
+            result[index++] = curr;
+
+            // process its dependencies
+            for (Integer next : adj.get(curr)){
+                dependency[next]--; // dependency decreased
+                if(dependency[next] == 0) queue.offer(next);
+            }
+        }
+        return count < numCourses ? new int[] {} : result;
+    }
 }
