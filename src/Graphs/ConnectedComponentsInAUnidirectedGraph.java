3464bd2b50705c23c0b2261569274dda40999e14
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-24 20:22:14 +0530

    used DFS based solution to return connected components

--- src/Graphs/ConnectedComponentsInAUnidirectedGraph.java
+++ src/Graphs/ConnectedComponentsInAUnidirectedGraph.java
@@ -1,4 +1,41 @@
 package Graphs;
 
-public class ConnectedComponentsInAUnidirectedGrpah {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectedComponentsInAUnidirectedGraph {
+    public ArrayList<ArrayList<Integer>> getComponents(int v, int[][] edges) {
+        List<List<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < v; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int [] e : edges) {
+            graph.get(e[0]).add(e[1]);
+            graph.get(e[1]).add(e[0]);
+        }
+
+        boolean [] visited = new boolean[v];
+        ArrayList<ArrayList<Integer>> connected = new ArrayList<>();
+
+        for (int i = 0; i < v; i++) {
+            if(!visited[i]){
+               ArrayList<Integer> component = new ArrayList<>();
+               dfs(graph,component,i,visited);
+               connected.add(component);
+            }
+        }
+        return connected;
+    }
+
+    private void dfs(List<List<Integer>> graph, ArrayList<Integer> component, int node, boolean[] visited)
+    {
+        visited[node] = true;
+        component.add(node);
+
+        for (int neighbour : graph.get(node)) {
+            if(!visited[neighbour]) dfs(graph,component,neighbour,visited);
+        }
+    }
 }
