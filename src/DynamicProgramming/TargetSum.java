3c6a4ffe88a5f23aa246b5dbf6b71d5acfa7aa48
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-27 18:17:42 +0530

    add dp based solution to find ways to reach target with + and - values for each item

--- src/DynamicProgramming/TargetSum.java
+++ src/DynamicProgramming/TargetSum.java
@@ -1,9 +1,9 @@
-package BackTracking;
+package DynamicProgramming;
 
 public class TargetSum {
     private int count;
 
-    public int findTargetSumWays(int[] nums, int target) {
+    public int findTargetSumWaysBrute(int[] nums, int target) {
         count = 0;
         backTrack(nums,target,0);
         return count;
@@ -18,5 +18,23 @@ public class TargetSum {
         int value = nums[i];
         backTrack(nums,target-value,i+1);
         backTrack(nums,target+value,i+1);
+    }
+
+    public int findTargetSumWays(int[] nums, int target) {
+        int totalSum = 0;
+        for (int item : nums) totalSum+= item;
+        // check validation can we break it into two + and - groups?
+        if((target+totalSum) %2 != 0 || Math.abs(target) > totalSum) return 0;
+
+        int subSetSum = (totalSum+target)/2;
+        int [] dp = new int[subSetSum+1];
+        dp[0] = 1;
+
+        for (int item: nums){
+            for (int i = subSetSum; i >= item ; i--) {
+                dp[i] += dp[i-item];
+            }
+        }
+        return dp[subSetSum];
     }
 }
