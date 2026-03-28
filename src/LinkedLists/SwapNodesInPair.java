17efd838aa995f5c8e480e4199fb0a1ea70c7b99
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-28 20:43:17 +0530

    add method to swap to nodes in one linked list

--- src/LinkedLists/SwapNodesInPair.java
+++ src/LinkedLists/SwapNodesInPair.java
@@ -1,4 +1,22 @@
 package LinkedLists;
 
 public class SwapNodesInPair {
+    public ListNode swapPairs(ListNode head) {
+        ListNode curr = head;
+        ListNode result = new ListNode(0,head);
+        ListNode dummy = result;
+
+        while (curr != null && curr.next != null){
+            ListNode npn = curr.next.next;
+            ListNode second = curr.next;
+
+            second.next = curr;
+            curr.next = npn;
+            dummy.next = second;
+
+            dummy = curr;
+            curr = npn;
+        }
+        return result.next;
+    }
 }
