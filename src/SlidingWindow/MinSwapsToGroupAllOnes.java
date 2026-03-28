22479838d3fb0c368be70a18e58eef330e05358e
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-28 20:40:25 +0530

    add sliding window to find min swaps req to group all one

--- src/SlidingWindow/MinSwapsToGroupAllOnes.java
+++ src/SlidingWindow/MinSwapsToGroupAllOnes.java
@@ -1,4 +1,26 @@
 package SlidingWindow;
 
 public class MinSwapsToGroupAllOnes {
+    public int minSwaps(int[] nums) {
+        int totalOne = 0;
+        for(int item : nums) {
+            if(item == 1) totalOne++;
+        }
+
+        int left = 0;
+        int zeros = 0;
+        int min = Integer.MAX_VALUE;
+
+        for (int right = 0; right < nums.length; right++) {
+            if(nums[right] == 0) zeros ++;
+            int window = right-left+1;
+
+            if(window == totalOne){
+                min = Math.min(min,zeros);
+                if(nums[left] == 0) zeros --;
+                left++;
+            }
+        }
+        return min == Integer.MAX_VALUE ? -1 : min;
+    }
 }
