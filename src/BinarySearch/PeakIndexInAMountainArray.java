edb44b92789c677c14c9ebc56f72603ac6258a4e
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-28 20:35:22 +0530

    add a binary search method to find max peak of an array

--- src/BinarySearch/PeakIndexInAMountainArray.java
+++ src/BinarySearch/PeakIndexInAMountainArray.java
@@ -1,4 +1,21 @@
 package BinarySearch;
 
 public class PeakIndexInAMountainArray {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                // increasing → peak is on right
+                left = mid + 1;
+            } else {
+                // decreasing → peak is at mid or left
+                right = mid;
+            }
+        }
+        return left;
+    }
 }
