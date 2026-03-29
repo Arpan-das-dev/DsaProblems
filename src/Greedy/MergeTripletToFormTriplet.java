d7f5bbfd6adae2cf52c5b20e13c50b30fa00018d
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-29 17:46:48 +0530

    add greedy based solution to find can triplet can be made or not

--- src/Greedy/MergeTripletToFormTriplet.java
+++ src/Greedy/MergeTripletToFormTriplet.java
@@ -1,4 +1,18 @@
 package Greedy;
 
 public class MergeTripletToFormTriplet {
+    public boolean mergeTriplets(int[][] triplets, int[] target) {
+        boolean firstMax = false, secondMax = false,thirdMax=false;
+
+        for(int[] trip:triplets)   {
+            if(trip[0]>target[0]||trip[1]>target[1]||trip[2]>target[2]) continue;
+
+            if(trip[0]==target[0]) firstMax=true;
+            if(trip[1]==target[1]) secondMax=true;
+            if(trip[2]==target[2]) thirdMax=true;
+            if(firstMax && secondMax && thirdMax) return true;
+
+        }
+        return firstMax && secondMax && thirdMax;
+    }
 }
