838e1f27c6fae7f986c277ebb02ce0d2f5809556
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-29 17:37:18 +0530

    designed a data structure to find kth highest value from a stream

--- src/Heaps/KthLargest.java
+++ src/Heaps/KthLargest.java
@@ -1,4 +1,23 @@
 package Heaps;
 
+import java.util.PriorityQueue;
+
 public class KthLargest {
+    private final int k;
+    private final PriorityQueue<Integer> stream;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.stream = new PriorityQueue<>();
+        for (int val: nums) add(val);
+    }
+
+    public int add(int val) {
+        stream.offer(val);
+
+        if(stream.size() > k){
+            stream.poll();
+        }
+        return stream.isEmpty() ? 0: stream.peek();
+    }
 }
