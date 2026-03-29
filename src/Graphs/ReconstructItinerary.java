e38fd885a4918d52d9693bca55aa0287146d560f
Arpan-das-dev <dasarpan915@gmail.com>
2026-03-29 17:57:08 +0530

    solved Eulerian Path problem on a directed graph to find correct itinerary

--- src/Graphs/ReconstructItinerary.java
+++ src/Graphs/ReconstructItinerary.java
@@ -1,4 +1,27 @@
 package Graphs;
 
-public class ReconstructItenrary {
+import java.util.*;
+
+public class ReconstructItinerary {
+    public List<String> findItinerary(List<List<String>> tickets) {
+        Map<String,PriorityQueue<String>> graph = new HashMap<>();
+
+        tickets.forEach(item -> graph
+                .computeIfAbsent(item.getFirst(), k -> new PriorityQueue<>())
+                .offer(item.get(1)));
+
+        List<String> result = new ArrayList<>();
+        dfs("JFK",graph,result);
+        return result;
+    }
+
+    private void dfs(String src, Map<String, PriorityQueue<String>> graph, List<String> result) {
+        PriorityQueue<String> qu = graph.get(src);
+
+        while (qu != null && !qu.isEmpty()){
+            String next = qu.poll();
+            dfs(next,graph,result);
+        }
+        result.addFirst(src);
+    }
 }
