d5e67e088c95a4ee5b7860ff03ca7d394234fe80
Arpan-das-dev <dasarpan915@gmail.com>
2026-04-04 20:20:21 +0530

    add dp based solution to find if the array can be divided into two subset of same sum

--- src/DynamicProgramming/PartitionEqualSubsetSum.java
+++ src/DynamicProgramming/PartitionEqualSubsetSum.java
@@ -1,4 +1,21 @@
 package DynamicProgramming;
 
-public class PartionEqualSubsetSum {
+public class PartitionEqualSubsetSum {
+    public boolean canPartition(int[] nums) {
+       int sum = 0;
+       for (int item : nums) sum+= item;
+       // if not even sum can not be partitioned
+       if(sum %2 != 0) return false;
+       int target = sum/2;
+
+       boolean [] dp = new boolean[target+1];
+       dp[0] = true;
+
+       for (int item: nums){
+           for (int i = target; i >= item ; i--) {
+               dp[i] = dp[i] || dp[i-item];
+           }
+       }
+       return dp[target];
+    }
 }
