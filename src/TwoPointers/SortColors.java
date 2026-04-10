a983dda7441ecedad44fb8968aafce639248063d
Arpan-das-dev <dasarpan915@gmail.com>
2026-04-10 18:50:31 +0530

    add a dutch flag algorithm to sort colors

--- src/TwoPointers/SortColors.java
+++ src/TwoPointers/SortColors.java
@@ -1,4 +1,27 @@
 package TwoPointers;
 
 public class SortColors {
+    public void sortColors(int[] nums) {
+        if(nums == null || nums.length == 0) return;
+
+        int low = 0, mid = 0, high = nums.length-1;
+        while (mid <= high){
+            if(nums[mid] == 0){
+                swapIt(nums,low,mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swapIt(nums,mid,high);
+                high--;
+            }
+        }
+    }
+
+    private void swapIt(int[] nums, int low, int mid) {
+        int temp = nums[low];
+        nums[low] = nums[mid];
+        nums[mid] = temp;
+    }
 }
