9fedf7eca352ea2b9d7250f4cc582d894fba2931
Arpan-das-dev <dasarpan915@gmail.com>
2026-04-10 18:59:28 +0530

    add binary search method to guess the number user picked

--- src/BinarySearch/GuessTheNumber.java
+++ src/BinarySearch/GuessTheNumber.java
@@ -1,4 +1,30 @@
 package BinarySearch;
 
 public class GuessTheNumber {
+    private final int picked;
+    public GuessTheNumber(int picked){
+        this.picked = picked;
+    }
+    public int guessNumber(int n) {
+        int max = n;
+        int min = 0;
+        while (min <= max){
+            int mid = min + (max-min)/2;
+            int res = guess(mid);
+
+            switch (res){
+                case 0 -> {
+                    return mid;
+                }
+                case -1 -> max = mid-1;
+                default -> min = mid+1;
+            }
+        }
+        return -1;
+    }
+
+    private int guess(int num) {
+        return Integer.compare(picked, num);
+    }
+
 }
