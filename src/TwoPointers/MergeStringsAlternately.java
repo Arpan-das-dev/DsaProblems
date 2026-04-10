256039df56d4cd7b14710364882ddf37c46b057e
Arpan-das-dev <dasarpan915@gmail.com>
2026-04-10 19:03:25 +0530

    add two pointer approach to generate string with alternate chars

--- src/TwoPointers/MergeStringsAlternately.java
+++ src/TwoPointers/MergeStringsAlternately.java
@@ -1,4 +1,21 @@
 package TwoPointers;
 
 public class MergeStringsAlternately {
+    public String mergeAlternately(String word1, String word2) {
+        int min = Math.min(word1.length(), word2.length());
+        String bigger = word1.length() > word2.length() ? word1 : word2;
+
+        StringBuilder res = new StringBuilder();
+        int idx = 0;
+        while (idx < min) {
+            res.append(word1.charAt(idx));
+            res.append(word2.charAt(idx));
+            idx++;
+        }
+
+        if (idx < bigger.length()) {
+            res.append(bigger.substring(idx));
+        }
+        return res.toString();
+    }
 }
