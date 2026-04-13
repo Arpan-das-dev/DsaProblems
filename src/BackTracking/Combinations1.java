c974472cd1971dd368dbd18bc7b5d44d68d0f87a
Arpan-das-dev <dasarpan915@gmail.com>
2026-04-13 20:34:27 +0530

    add recursive backtracking to detect all distinct combination of k size

--- src/BackTracking/Combinations1.java
+++ src/BackTracking/Combinations1.java
@@ -1,4 +1,25 @@
 package BackTracking;
 
-public class Combiantions1 {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Combinations1 {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        backTrack(1,n,k,result,new ArrayList<Integer>());
+        return result;
+    }
+
+    private void backTrack(int integer, int n, int k, List<List<Integer>> result, ArrayList<Integer> integers) {
+        if (integers.size() == k) {
+            result.add(new ArrayList<>(integers));
+            return;
+        }
+
+        for (int i = integer; i <= n - (k - integers.size()) + 1; i++) { // early pruning
+            integers.add(i);
+            backTrack(i + 1, n, k, result, integers);
+            integers.removeLast();
+        }
+    }
 }
