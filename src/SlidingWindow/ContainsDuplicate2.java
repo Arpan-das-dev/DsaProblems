a6adfa0f5dcb2a93aa70ceddf31875c6c8058ee7
Arpan-das-dev <dasarpan915@gmail.com>
2026-04-13 20:21:46 +0530

    add fixed sized sliding window with hashset to detect duplicate in k range

--- src/SlidingWindow/ContainsDuplicate2.java
+++ src/SlidingWindow/ContainsDuplicate2.java
@@ -1,4 +1,20 @@
 package SlidingWindow;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate2 {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> unique = new HashSet<>();
+        int left = 0;
+
+        for (int curr : nums) {
+            if (unique.contains(curr)) return true;
+            else if (unique.size() > k) {
+                unique.remove(nums[left]);
+                left++;
+            } else unique.add(curr);
+        }
+        return false;
+    }
 }
