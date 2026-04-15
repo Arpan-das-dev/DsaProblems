4b374f3935c5f539ed9edb3371d1454b24ec4ba5
Arpan-das-dev <dasarpan915@gmail.com>
2026-04-15 20:38:25 +0530

    implemented stack using only one queue

--- src/Heaps/MyStack.java
+++ src/Heaps/MyStack.java
@@ -1,4 +1,32 @@
 package Heaps;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class MyStack {
+    private final Queue<Integer> stack;
+
+    public MyStack() {
+        this.stack = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        stack.offer(x);
+        int size = stack.size();
+        for (int i = 0; i < size-1; i++) {
+            stack.offer(stack.poll());
+        }
+    }
+
+    public int pop() {
+        return stack.isEmpty() ? 0: stack.poll();
+    }
+
+    public int top() {
+        return stack.isEmpty() ? 0:stack.peek();
+    }
+
+    public boolean empty() {
+        return stack.isEmpty();
+    }
 }
