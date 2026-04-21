066674998ed16995882af9ccb2112e857a705b73
Arpan-das-dev <dasarpan915@gmail.com>
2026-04-21 19:28:12 +0530

    add greedy approach with treemap to solve hands of straights

--- src/Greedy/HandsOfStraights.java
+++ src/Greedy/HandsOfStraights.java
@@ -1,4 +1,31 @@
 package Greedy;
 
+import java.util.TreeMap;
+
 public class HandsOfStraights {
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+        // invalid size edge case
+        if(hand.length % groupSize != 0) return false;
+        // store frequency
+        TreeMap<Integer,Integer> cardFreq = new TreeMap<>();
+        for (int card: hand){
+            cardFreq.put(card,cardFreq.getOrDefault(card,0)+1);
+        }
+
+        while (!cardFreq.isEmpty()){
+            int firstKey = cardFreq.firstKey();
+            for (int i = 0; i< groupSize;i++){
+                int next = firstKey+i;
+                if(!cardFreq.containsKey(next)) return false;
+
+                int count = cardFreq.get(next);
+                if(count == 1){
+                    cardFreq.remove(next);
+                }else {
+                    cardFreq.put(next,count-1);
+                }
+            }
+        }
+        return true;
+    }
 }
